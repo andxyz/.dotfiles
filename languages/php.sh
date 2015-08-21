96a28f344bff626783dcae61b52cb605343eb864
Andrew Stevens <andxyz.stevens@gmail.com>
2015-08-21 13:50:35 -0400

    fix the homebrew php warning

--- languages/php.sh
+++ languages/php.sh
@@ -5,7 +5,7 @@ set -ex
 ### php
 brew tap homebrew/dupes
 brew tap homebrew/versions
-brew tap josegonzalez/homebrew-php
+brew tap josegonzalez/php
 #### you should read the following for php
 ##### `brew options php54`
 ##### `brew info php54`
@@ -13,8 +13,8 @@ brew tap josegonzalez/homebrew-php
 ##### `brew info php55`
 ##### `open http://justinhileman.info/article/reinstalling-php-on-mac-os-x/`
 ##### `open http://blog.frd.mn/install-nginx-php-fpm-mysql-and-phpmyadmin-on-os-x-mavericks-using-homebrew/`
-brew install php55 --with-homebrew-curl --with-homebrew-openssl --with-fpm --with-mysql --with-pgsql
-brew install php56 --with-homebrew-curl --with-homebrew-openssl --without-snmp --with-fpm --with-mysql --with-pgsql
+brew install josegonzalez/php/php55 --with-homebrew-curl --with-homebrew-openssl --with-fpm --with-mysql --with-pgsql
+brew install josegonzalez/php/php56 --with-homebrew-curl --with-homebrew-openssl --without-snmp --with-fpm --with-mysql --with-pgsql
 
 ### php addons to the addons
 ##### brew options imagemagick
