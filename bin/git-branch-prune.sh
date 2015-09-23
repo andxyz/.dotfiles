568f03cbfa270270879bc24cc64eb302caa2ea1e
Andrew Stevens <andxyz.stevens@gmail.com>
2015-09-23 15:56:06 -0400

    struggling to get bin folder underversion control

--- bin/git-branch-prune.sh
+++ bin/git-branch-prune.sh
@@ -2,8 +2,8 @@
 # Usage: git-branch-prune
 # Delete all merged branches.
 
-# prune github and local
-# git remote prune origin && git branch --merged | grep -v "\*" | grep -v master | xargs -n 1 git branch --delete
+# prune local
+# git branch --merged | grep -v "\*" | xargs -n 1 git branch --delete
 
-git remote prune --dry-run origin &&
-git branch --merged | grep -v "\*" | grep -v master | xargs -n 1 echo " * [would delete local branch]"
+# prune github and local
+git remote prune origin && git branch --merged | grep -v "\*" | grep -v master | xargs -n 1 git branch --delete
