e2b54863f3f7af027f46b824b4c6fbab26b9d47e
Andrew Stevens <andxyz.stevens@gmail.com>
2015-12-14 17:03:13 -0500

    Update npm setup

--- languages/nodejs.sh
+++ languages/nodejs.sh
@@ -7,7 +7,8 @@ git clone https://github.com/creationix/nvm.git ~/.nvm
 
 source $HOME/.nvm/nvm.sh
 
-# nvm ls-remote
+nvm ls-remote
+nvm install v5.2.0
 nvm install v0.12.2
 nvm install v0.11.13
 nvm install v0.11.12
@@ -19,21 +20,24 @@ nvm install v0.10.25
 
 ## set our dev machine defaults
 ### for future shells
-nvm alias default v0.12.2
+nvm alias default v5.2.0
 ### for this shell right now. now now? like now? yes, right now now.
-nvm use v0.12.2
+nvm use v5.2.0
 
 ## install some decent global libraries on the default node
-#### npm install -g npm --silent &&
+#### npm update -g --silent npm
+npm update -g --silent npm
+npm install -g --silent npm-shrinkwrap
 npm install -g --silent node-inspector
-npm install -g --silent bower
+# npm install -g --silent bower
 npm install -g --silent grunt
 npm install -g --silent grunt-cli
 npm install -g --silent gulp
 npm install -g --silent jshint
 
-# npm install -g --silent handlebars
-# npm install -g --silent less
+## front end tools
+npm install -g --silent handlebars
+npm install -g --silent less
 
 # npm install -g --silent pageres
 # npm install -g --silent supervisor
@@ -67,5 +71,9 @@ npm install -g --silent jshint
 ## show off the whizbangs
 npm list -g --depth=0
 nvm ls
+
+echo 'I recommend forcing exact versions for working with emberjs'
+echo 'example:   npm install -g --silent ember-cli@1.13.13'
+echo 'example:   npm install -g --silent bower@1.6.5'
 
 exit 0
