74a2f603bba1445bdf653e86656f528ecd6f170e
Andrew Stevens <andxyz.stevens@gmail.com>
2016-07-05 13:58:41 -0400

    update php configs

--- languages/php.sh
+++ languages/php.sh
@@ -18,15 +18,13 @@ brew tap homebrew/php
 #
 ##### `brew options homebrew/php/php56`
 ##### `brew info homebrew/php/php56`
-##### `open http://justinhileman.info/article/reinstalling-php-on-mac-os-x/`
-##### `open http://blog.frd.mn/install-nginx-php-fpm-mysql-and-phpmyadmin-on-os-x-mavericks-using-homebrew/`
-brew install homebrew/php/php55 --with-homebrew-curl --with-homebrew-openssl --with-fpm --with-mysql --with-pgsql
+
+# brew install homebrew/php/php55 --with-homebrew-curl --with-homebrew-openssl --with-fpm --with-mysql --with-pgsql
 brew install homebrew/php/php56 --with-homebrew-curl --with-homebrew-openssl --without-snmp --with-fpm --with-mysql --with-postgresql
 
 ### php addons to the addons
 ##### brew options imagemagick
 ##### brew info imagemagick
 brew install 'imagemagick'
-
 
 exit 0
