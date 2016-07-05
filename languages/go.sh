20cbac1bbb618dc27de954d6cfd85bdad1b5b09d
Andrew Stevens <andxyz.stevens@gmail.com>
2016-07-05 13:59:00 -0400

    Update golang configs

--- languages/go.sh
+++ languages/go.sh
@@ -2,6 +2,13 @@
 
 set -ex
 
-echo "install go stuff goes here"
+brew install go
+# golang https://golang.org/doc/install or `brew install go`
+export -- GOPATH=$HOME/code/gowork
+export -- PATH=$PATH:/usr/local/go/bin
+export -- PATH=${PATH}:${GOPATH}/bin
+## Note: GOROOT must be set only when installing to a custom location.
+#export GOROOT=$HOME/go
+#export PATH=$PATH:$GOROOT/bin
 
 exit 0
