5b4a07b053dcd19c4fd4757df4cbb55c4d32e7b5
Andrew Stevens <andxyz.stevens@gmail.com>
2016-08-15 10:08:48 -0400

    Update osx tweaks

--- bin/osx_tweaks.sh
+++ bin/osx_tweaks.sh
@@ -2,12 +2,31 @@
 
 set -ex
 
-chflags nohidden ~/Library/
-defaults write com.apple.screencapture name "screenshot"
-mkdir $HOME/Pictures/_screenshots/
-defaults write com.apple.screencapture location $HOME/Pictures/_screenshots/
+# see https://github.com/mathiasbynens/dotfiles/blob/master/.macos
 
-# see https://github.com/mathiasbynens/dotfiles
-# see https://github.com/sindresorhus/quick-look-plugins
+# Show the ~/Library folder
+sudo chflags nohidden ~/Library
+
+# Show the /Volumes folder
+sudo chflags nohidden /Volumes
+
+# Save screenshots to the desktop
+mkdir ${HOME}/Pictures/_screenshots/
+defaults write com.apple.screencapture location -string "${HOME}/Pictures/_screenshots/"
+
+# Set screenshots name
+defaults write com.apple.screencapture name "screenshot"
+
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+defaults write com.apple.screencapture type -string "png"
+
+# Disable shadow in screenshots
+defaults write com.apple.screencapture disable-shadow -bool true
+
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
+
+# Save to disk (not to iCloud) by default
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
 exit 0
