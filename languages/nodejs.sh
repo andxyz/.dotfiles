f0f2b6c9823ef5fe59728ccd8c97908607666577
Andrew Stevens <andxyz.stevens@gmail.com>
2016-11-08 13:32:18 -0500

    Try out diff-so-fancy, and update nodejs install

--- languages/nodejs.sh
+++ languages/nodejs.sh
@@ -1,43 +1,59 @@
 #!/usr/bin/env bash
-set -xe
 
-### see https://github.com/creationix/nvm
+set -x
+set -e
+
+## see https://github.com/creationix/nvm
 ! test -d ~/.nvm &&
 git clone https://github.com/creationix/nvm.git ~/.nvm
 
 source $HOME/.nvm/nvm.sh
 
 nvm ls-remote
-nvm install v5.2.0
-nvm install v0.12.2
-nvm install v0.11.13
-nvm install v0.11.12
-nvm install v0.11.14
-nvm install v0.10.31
-nvm install v0.10.28
-nvm install v0.10.26
-nvm install v0.10.25
+nvm install v6.9.1
+nvm install v5.11.1
+# nvm install v5.2.0
+# nvm install v0.12.2
+# nvm install v0.11.13
+# nvm install v0.11.12
+# nvm install v0.11.14
+# nvm install v0.10.31
+# nvm install v0.10.28
+# nvm install v0.10.26
+# nvm install v0.10.25
 
 ## set our dev machine defaults
 ### for future shells
-nvm alias default v5.2.0
+nvm alias default v6.9.1
 ### for this shell right now. now now? like now? yes, right now now.
-nvm use v5.2.0
+nvm use v6.9.1
 
 ## install some decent global libraries on the default node
 #### npm update -g --silent npm
 npm update -g --silent npm
-npm install -g --silent npm-shrinkwrap
-npm install -g --silent node-inspector
-# npm install -g --silent bower
-npm install -g --silent grunt
-npm install -g --silent grunt-cli
-npm install -g --silent gulp
-npm install -g --silent jshint
+npm install -g --silent --no-progress --quiet --depth=0 npm-shrinkwrap
+npm install -g --silent --no-progress --quiet --depth=0 node-inspector
+npm install -g --silent --no-progress --quiet --depth=0 bower
+npm install -g --silent --no-progress --quiet --depth=0 grunt
+npm install -g --silent --no-progress --quiet --depth=0 grunt-cli
+npm install -g --silent --no-progress --quiet --depth=0 gulp
+
+## linting
+npm install -g --silent --no-progress --quiet --depth=0 babel-eslint
+npm install -g --silent --no-progress --quiet --depth=0 jshint
 
 ## front end tools
-npm install -g --silent handlebars
-npm install -g --silent less
+npm install -g --silent --no-progress --quiet --depth=0 handlebars
+npm install -g --silent --no-progress --quiet --depth=0 less
+
+## chrome tools
+npm install -g --silent --no-progress --quiet --depth=0 yo
+npm install -g --silent --no-progress --quiet --depth=0 generator-chrome-extension
+
+## tools
+npm install -g --silent --no-progress --quiet --depth=0 diff-so-fancy
+npm install -g --silent --no-progress --quiet --depth=0 ios-sim
+npm install -g --silent --no-progress --quiet --depth=0 ios-deploy
 
 # npm install -g --silent pageres
 # npm install -g --silent supervisor
@@ -68,12 +84,12 @@ npm install -g --silent less
 ### supervisor
 ### watchify
 
-## show off the whizbangs
-npm list -g --depth=0
+echo "show off the whizbangs"
+npm list -g --no-progress --quiet --depth=0
 nvm ls
 
-echo 'I recommend forcing exact versions for working with emberjs'
-echo 'example:   npm install -g --silent ember-cli@1.13.13'
-echo 'example:   npm install -g --silent bower@1.6.5'
+# echo 'I recommend forcing exact versions for working with emberjs'
+# echo 'example:   npm install -g --silent ember-cli@1.13.13'
+# echo 'example:   npm install -g --silent bower@1.6.5'
 
 exit 0
