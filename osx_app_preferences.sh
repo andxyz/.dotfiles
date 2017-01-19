cb3c5055eb85e6e56fdb6cf455a63fe14f8012ac
Andrew Stevens <andxyz.stevens@gmail.com>
2017-01-19 13:58:10 -0500

    Better setup for making my default editor open for a bunch of crazy file types

--- osx_app_preferences.sh
+++ osx_app_preferences.sh
@@ -3,132 +3,137 @@
 set -x
 set -e
 
-export -- LSTOOL_HOME=/Users/andxyz/Library/PreferencePanes/RCDefaultApp.prefPane/Contents/Resources/
+LSTOOL_HOME='/Users/andxyz/Library/PreferencePanes/RCDefaultApp.prefPane/Contents/Resources/'
+EDITOR_PATH='/Applications/Sublime Text 3.app'
 
-$LSTOOL_HOME/lstool write "extension" ".textmate_init"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "adp" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "asa" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "bash"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "bash_login"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "bash_logout" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "bash_profile"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "bashrc"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "bat" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "bib" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "bsh" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "build" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "capfile" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "cgi" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "cpt" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "cpy" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "cs"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "css" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "css.erb" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "ctp" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "d" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "ddl" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "di"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "dml" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "dtml"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "el"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "emakefile" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "Emakefile" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "erb" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "erbsql"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "erl" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "fcgi"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "gemspec" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "geojson" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "GNUmakefile" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "groovy"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "gvy" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "hrl" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "hs"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "htc" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "inc" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "inl" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "irbrc" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "js"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "json"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "jsp" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "jsx" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "lhs" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "lisp"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "lua" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "Makefile"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "makefile"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "markdn"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "markdown"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "matlab"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "md"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "mdown" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "ml"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "mli" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "mll" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "mly" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "mud" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "OCamlMakefile" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "opml"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "pod" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "profile" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "properties"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "pt"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "py"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "pyw" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "rake"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "Rakefile"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "rb"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "rbw" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "rbx" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "re"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "rhtml" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "rjs" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "Rprofile"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "rpy" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "rss" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "rxml"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "scala" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "scm" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "SConscript"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "SConstruct"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "sconstruct"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "Sconstruct"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "sh"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "sql" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "ss"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "sublime-build" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "sublime-keymap"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "sublime-menu"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "sublime-project" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "sublime-settings"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "sublime-snippet" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "sublime-theme" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "svg" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "t" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "tcl" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "tcsh"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "textile" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "tld" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "tmLanguage"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "tmpl"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "tmPreferences" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "tmSnippet" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "tmTheme" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "tpl" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "vcproj"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "xhtml" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "XHTML" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "xsd" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "xsl" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "xslt"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "yaml"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "yaws"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "extension" "yml" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "uti" "com.apple.xcode.zsh-script"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "uti" "net.daringfireball.markdown" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "uti" "public.perl-script"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "uti" "public.python-script"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "uti" "public.ruby-script"  '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "uti" "public.script" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "uti" "public.shell-script" '/Applications/Sublime Text 3.app'
-$LSTOOL_HOME/lstool write "uti" "public.source-code"  '/Applications/Sublime Text 3.app'
+${LSTOOL_HOME}/lstool --help
+${LSTOOL_HOME}/lstool apps | grep -i sublime
+${LSTOOL_HOME}/lstool apps | grep -i vim
+
+${LSTOOL_HOME}/lstool write "extension" ".textmate_init" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "adp" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "asa" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "bash" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "bash_login" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "bash_logout" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "bash_profile" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "bashrc" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "bat" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "bib" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "bsh" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "build" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "capfile" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "cgi" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "cpt" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "cpy" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "cs" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "css" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "css.erb" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "ctp" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "d" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "ddl" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "di" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "dml" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "dtml" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "el" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "emakefile" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "Emakefile" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "erb" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "erbsql" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "erl" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "fcgi" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "gemspec" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "geojson" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "GNUmakefile" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "groovy" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "gvy" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "hrl" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "hs" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "htc" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "inc" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "inl" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "irbrc" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "js" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "json" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "jsp" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "jsx" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "lhs" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "lisp" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "lua" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "Makefile" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "makefile" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "markdn" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "markdown" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "matlab" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "md" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "mdown" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "ml" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "mli" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "mll" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "mly" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "mud" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "OCamlMakefile" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "opml" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "pod" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "profile" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "properties" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "pt" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "py" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "pyw" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "rake" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "Rakefile" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "rb" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "rbw" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "rbx" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "re" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "rhtml" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "rjs" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "Rprofile" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "rpy" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "rss" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "rxml" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "scala" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "scm" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "SConscript" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "SConstruct" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "sconstruct" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "Sconstruct" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "sh" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "sql" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "ss" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "sublime-build" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "sublime-keymap" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "sublime-menu" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "sublime-project" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "sublime-settings" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "sublime-snippet" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "sublime-theme" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "svg" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "t" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "tcl" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "tcsh" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "textile" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "tld" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "tmLanguage" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "tmpl" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "tmPreferences" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "tmSnippet" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "tmTheme" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "tpl" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "vcproj" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "xhtml" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "XHTML" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "xsd" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "xsl" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "xslt" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "yaml" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "yaws" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "extension" "yml" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "uti" "com.apple.xcode.zsh-script" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "uti" "net.daringfireball.markdown" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "uti" "public.perl-script" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "uti" "public.python-script" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "uti" "public.ruby-script" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "uti" "public.script" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "uti" "public.shell-script" "${EDITOR_PATH}"
+${LSTOOL_HOME}/lstool write "uti" "public.source-code" "${EDITOR_PATH}"
