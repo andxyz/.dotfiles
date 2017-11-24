97da68db20b8ec3ab33b105d12e930c9eb302f7b
Andrew Stevens <andxyz.stevens@gmail.com>
2017-11-24 15:36:55 -0500

    2017ify python

--- languages/python.sh
+++ languages/python.sh
@@ -7,10 +7,21 @@ echo "install python stuff goes here"
 
 brew install python --framework
 ### brew install python3 --framework
-pip install --upgrade pip
-pip install --upgrade virtualenv
-pip install --upgrade paramiko
-pip install --upgrade machobot
-pip install --upgrade mitmproxy
+pip install --no-cache-dir --no-binary --upgrade pip
+pip install --no-cache-dir --no-binary --upgrade virtualenv
+pip install --no-cache-dir --no-binary --upgrade paramiko
+pip install --no-cache-dir --no-binary --upgrade machobot
+pip install --no-cache-dir --no-binary --upgrade mitmproxy
+# aws repl
+pip install --no-cache-dir --no-binary --upgrade git+https://github.com/awslabs/aws-shell --upgrade six
+# docker repl
+pip install --no-cache-dir --no-binary --upgrade git+https://github.com/j-bennet/wharfee.git --upgrade six
+# kube repl
+pip install --no-cache-dir --no-binary --upgrade git+https://github.com/cloudnativelabs/kube-shell --upgrade six
+
+
+# pip install --no-cache-dir --no-binary --upgrade httpie
+# pip install --no-cache-dir --no-binary --upgrade pgcli
+# pip install --no-cache-dir --no-binary --upgrade mycli
 
 exit 0
