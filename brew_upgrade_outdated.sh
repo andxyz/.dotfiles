a11b257005e4136575452e0ff5df1ffea5215ff8
Andrew Stevens <andxyz.stevens@gmail.com>
2019-01-18 16:39:06 -0500

    homebew cleanup outdated script

--- brew_upgrade_outdated.sh
+++ brew_upgrade_outdated.sh
@@ -10,32 +10,30 @@ set -e
 ### first off, update all brew meta data crap
 ## house cleaning round 1
 brew update
-brew cleanup -s && brew prune
-brew cask cleanup
+brew cleanup -s
 
 brew tap 'homebrew/aliases'
-brew tap 'homebrew/apache'
 brew tap 'homebrew/bundle'
 brew tap 'homebrew/dev-tools'
-brew tap 'homebrew/php'
-brew tap 'homebrew/nginx'
-brew tap 'homebrew/science'
 brew tap 'homebrew/services'
 brew tap 'universal-ctags/universal-ctags'
 brew tap 'samdmarshall/formulae'
 
+brew untap 'homebrew/apache' || true # was deprecated. This tap is now empty as all its formulae were migrated.
 brew untap 'homebrew/binary' || true # was deprecated. This tap is now empty as all its formulae were migrated.
 brew untap 'homebrew/completions' || true # was deprecated. This tap is now empty as all its formulae were migrated.
 brew untap 'homebrew/dupes' || true # was deprecated. This tap is now empty as all its formulae were migrated.
 brew untap 'homebrew/fuse' || true # was deprecated. This tap is now empty as all its formulae were migrated.
+brew untap 'homebrew/nginx' || true # was deprecated. This tap is now empty as all its formulae were migrated.
+brew untap 'homebrew/php' || true # was deprecated. This tap is now empty as all its formulae were migrated.
 brew untap 'homebrew/python' || true # was deprecated. This tap is now empty as all its formulae were migrated.
+brew untap 'homebrew/science' || true # was deprecated. This tap is now empty as all its formulae were migrated.
 brew untap 'homebrew/tex' || true # was deprecated. This tap is now empty as all its formulae were migrated.
 brew untap 'homebrew/versions' || true # was deprecated. This tap is now empty as all its formulae were migrated.
 brew untap 'josegonzalez/php' || true # was deprecated. for 'homebrew/php'
 
 brew update
 brew cleanup -s
-brew prune
 
 # setup an ignore list
 # note I pay for alfred and name-mangler casks, so I don't want the lastest
@@ -43,6 +41,9 @@ IGNORE_LIST="carthage
 alfred
 name-mangler
 mysql@5.6
+mysql@5.5
+postgresql@9.5
+postgresql@9.6
 curl
 ffmpeg
 gnupg
@@ -82,10 +83,11 @@ update_the_cask_stuff() {
 }
 
 ## house cleaning round 2
-brew cleanup -s
-brew prune
-brew doctor
-brew cask cleanup
+brew_run_a_cleanup() {
+  brew cleanup -s
+  brew doctor
+  brew cask cleanup
+}
 
 TO_UPDATE_LIST=$(gcomm -12 <(brew leaves | sort) <(brew outdated | sort))
 echo "what brew knows needs updating:"
