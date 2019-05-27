24bae64f17db1234a60a19654a66a63a47d78a33
Andrew Stevens <andxyz.stevens@gmail.com>
2019-05-27 14:03:02 -0400

    bump python things for my private ansible project

--- languages/python.sh
+++ languages/python.sh
@@ -6,13 +6,17 @@ set -x
 echo "install python stuff goes here"
 
 brew install python --framework
-pip2 install --upgrade pip setuptools
-pip install --no-cache-dir --no-binary --upgrade virtualenv
+pip2 install --no-cache-dir --no-binary --upgrade virtualenv
+pip2 install --no-cache-dir --no-binary --upgrade pip setuptools
 
 # brew install python3 --framework
 # pip3 install --no-cache-dir --no-binary --upgrade virtualenv
+# pip3 install --no-cache-dir --no-binary --upgrade  pip setuptools
 
-# uhhh sure
+brew install pipenv
+
+# some tools
+# uhhhhh sure I guess
 pip install --no-cache-dir --no-binary --upgrade httpie
 pip install --no-cache-dir --no-binary --upgrade pgcli
 pip install --no-cache-dir --no-binary --upgrade mycli
