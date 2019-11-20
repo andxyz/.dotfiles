f1f767779b865775b6b387bc6d5db94e6695bebe
Andrew Stevens <andxyz.stevens@gmail.com>
2019-11-20 09:59:25 -0500

    Fixup python 2 install commands for osx10.4

--- languages/python.sh
+++ languages/python.sh
@@ -5,24 +5,26 @@ set -x
 
 echo "install python stuff goes here"
 
-brew install python --framework
+brew install python2
 pip2 install --no-cache-dir --no-binary --upgrade virtualenv
-pip2 install --no-cache-dir --no-binary --upgrade pip setuptools
+pip2 install --no-cache-dir --no-binary --upgrade pip
+pip2 install --no-cache-dir --no-binary --upgrade setuptools
 
-# brew install python3 --framework
+# brew install python3
 # pip3 install --no-cache-dir --no-binary --upgrade virtualenv
-# pip3 install --no-cache-dir --no-binary --upgrade  pip setuptools
+# pip3 install --no-cache-dir --no-binary --upgrade pip
+# pip3 install --no-cache-dir --no-binary --upgrade pipsetuptools
 
 brew install pipenv
 
 # some tools
 # uhhhhh sure I guess
-pip install --no-cache-dir --no-binary --upgrade httpie
-pip install --no-cache-dir --no-binary --upgrade pgcli
-pip install --no-cache-dir --no-binary --upgrade mycli
-pip install --no-cache-dir --no-binary --upgrade paramiko
-pip install --no-cache-dir --no-binary --upgrade machobot
-pip install --no-cache-dir --no-binary --upgrade mitmproxy
+pip3 install --no-cache-dir --no-binary --upgrade httpie
+pip3 install --no-cache-dir --no-binary --upgrade pgcli
+pip3 install --no-cache-dir --no-binary --upgrade mycli
+pip2 install --no-cache-dir --no-binary --upgrade paramiko
+pip2 install --no-cache-dir --no-binary --upgrade machobot
+pip2 install --no-cache-dir --no-binary --upgrade mitmproxy
 # aws repl
 pip install --no-cache-dir --no-binary --upgrade git+https://github.com/awslabs/aws-shell --upgrade six
 # docker repl
