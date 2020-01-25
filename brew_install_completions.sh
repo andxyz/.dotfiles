267025a3b66edf810ef4593b097514c830b3e9dd
Andrew Stevens <andxyz.stevens@gmail.com>
2020-01-25 13:50:32 -0500

    Update completions script for what I use

--- brew_install_completions.sh
+++ brew_install_completions.sh
@@ -6,6 +6,8 @@
 set -x
 set -e
 
+brew tap 'josh/tap'
+
 brew search completion | sort
 
 brew install 'apm-bash-completion'
@@ -16,18 +18,19 @@ brew install 'brew-cask-completion'
 brew install 'bundler-completion'
 # brew install 'cap-completion'
 brew install 'cargo-completion'
-brew install 'django-completion'
-brew install 'docker-completion'
-brew install 'docker-compose-completion'
-brew install 'docker-machine-completion'
-brew install 'fabric-completion'
+# brew install 'django-completion'
+# brew install 'docker-completion' # because docker already includes these completion scripts
+# brew install 'docker-compose-completion' # because docker-compose already includes completion scripts
+# brew install 'docker-machine-completion' # because docker-machine already includes completion scripts
+# brew install 'fabric-completion'
 brew install 'gem-completion'
-brew install 'gradle-completion'
-brew install 'grunt-completion'
+# brew install 'gradle-completion' # because bash-completion@2: because Differing version of same formula
+# brew install 'grunt-completion'
 # brew install 'homesick-completion'
+brew reinstall 'josh/tap/swift-completions' || brew install 'josh/tap/swift-completions'
 # brew install 'kitchen-completion'
 brew install 'launchctl-completion'
-brew install 'maven-completion'
+# brew install 'maven-completion'
 brew install 'mix-completion'
 brew install 'open-completion'
 brew install 'packer-completion'
@@ -42,6 +45,6 @@ brew install 'rustc-completion'
 # brew install 't-completion'
 brew install 'tmuxinator-completion'
 brew install 'vagrant-completion'
-brew install 'wpcli-completion'
+# brew install 'wpcli-completion'
 brew install 'yarn-completion'
 brew install 'zsh-completions'
