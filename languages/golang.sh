edfe410cbe223700733742d90f4f0d47a1a69c89
Andrew Stevens <andxyz.stevens@gmail.com>
2020-11-09 12:59:28 -0500

    golang 1.15 https://blog.golang.org/go1.15

--- languages/golang.sh
+++ languages/golang.sh
@@ -9,7 +9,8 @@ set -ex
 # brew install go@1.11
 # brew install go@1.12
 # brew install go@1.13
-brew install go@1.14
+# brew install go@1.14
+brew install go@1.15
 # golang https://golang.org/doc/install or `brew install go`
 export -- GOPATH=$HOME/code/gowork
 export -- PATH=$PATH:/usr/local/go/bin
