97c1c93a67f202fae1bf5593b005e14646d17773
Andrew Stevens <andxyz.stevens@gmail.com>
2021-04-03 17:45:05 -0400

    Fixup installing go instuctions

--- languages/golang.sh
+++ languages/golang.sh
@@ -11,10 +11,13 @@ set -ex
 # brew install go@1.13
 # brew install go@1.14
 brew install go@1.15
+
 # golang https://golang.org/doc/install or `brew install go`
-export -- GOPATH=$HOME/code/gowork
-export -- PATH=$PATH:/usr/local/go/bin
+#
+export -- GOPATH=${HOME}/code/gowork
+export -- PATH=${PATH}:/usr/local/opt/go@1.15/bin
 export -- PATH=${PATH}:${GOPATH}/bin
+
 ## Note: GOROOT must be set only when installing to a custom location.
 #export GOROOT=$HOME/go
 #export PATH=$PATH:$GOROOT/bin
