f4b0da3b7814ac1d4ea781eab33b8fe686162748
Andrew Stevens <andxyz.stevens@gmail.com>
2024-05-23 10:48:19 -0400

    Cleanup enable_db_debug_logger for rails 6 and 7

--- enable_db_debug_logger.rb
+++ enable_db_debug_logger.rb
@@ -12,34 +12,52 @@ if ::ActiveRecord
   ## enable_logger idea thanks to https://github.com/travisjeffery/dotfiles/blob/master/.railsrc
   require 'logger'
 
-  def enable_logger
+  def enable_logger!
     ::ActiveRecord::Base.logger = Logger.new(STDOUT)
-    ::ActiveRecord::Base.clear_active_connections!
+
+    if Rails.respond_to?(:gem_version) && Rails.gem_version >= Gem::Version.new('6.0.0')
+      ::ActiveRecord::Base.connection_handler.clear_active_connections!
+    else
+      ::ActiveRecord::Base.clear_active_connections!
+    end
+
     nil
   end
 
-  def disable_logger
+  def disable_logger!
     ::ActiveRecord::Base.logger = nil
-    ::ActiveRecord::Base.clear_active_connections!
+    if Rails.respond_to?(:gem_version) && Rails.gem_version >= Gem::Version.new('6.0.0')
+      ::ActiveRecord::Base.connection_handler.clear_active_connections!
+    else
+      ::ActiveRecord::Base.clear_active_connections!
+    end
+
     nil
   end
 
   # Hit all models for auto-completion
   def show_tables
-    # ::ActiveRecord::Base.establish_connection(
-    #   Rails.application.config_for(:database)
-    # )
+    ::ActiveRecord::Base.establish_connection(Rails.application.config_for(:database))
+
     ::ActiveRecord::Base.connection.tables.each do |t|
-      t.singularize.classify.constantize rescue nil
+      next if ['ar_internal_metadata', 'schema_migrations'].any?(t)
+
+      begin
+        puts t.singularize.classify.constantize.pretty_print_inspect
+      rescue StandardError => _exception
+        puts "skipping #{t}"
+      end
     end
+
+    nil
   end
 
-  # logging into console by default
-  enable_logger
+  enable_logger!
+  # show_tables
 end
 
 =begin
-env RUBYOPT="-I$HOME/code/rubygems.org -renable_db_debug_logger.rb" bundle exec rails console
+env RUBYOPT="-I$HOME/code/personal/andxyz-dotfiles/ -renable_db_debug_logger.rb" bundle exec rails console
 
 cd $HOME/thescore/sports/thescore-pulse/pulse &&
 (bundle check || bundle install) &&
