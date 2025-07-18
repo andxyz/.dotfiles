c34f9ae03f1a9580a2c6aab83cb9672993bb736e
Andrew Stevens <andxyz.stevens@gmail.com>
2025-07-18 16:12:09 -0400

    Cleanup fish config
    - add some useful aliases and cargo tools
    - fixup rbenv in fish

--- config/fish/conf.d/abbrev.fish
+++ config/fish/conf.d/abbrev.fish
@@ -1,9 +1,24 @@
+# https://fishshell.com/docs/current/cmds/abbr.html
+
+### ls
+### Colorize the ls output
+if command -q gls
+    # do things for macOS
+    abbr -a -- gls gls -F -h --group-directories-first --color=auto
+    abbr -a -- gll gls -l -h --group-directories-first --color=auto
+    abbr -a -- gla gls -A -h --group-directories-first --color=auto
+end
+
 if command -q eza
-    abbr -a ll eza -l --icons
-    abbr -a ls eza -g --icons
-    abbr -a la eza -g -a --icons
+    # if eza is installed
+    abbr -a -- ls eza -g --group-directories-first --color=auto --icons
+    abbr -a -- ll eza -l --group-directories-first --color=auto --icons
+    abbr -a -- la eza -g -a --group-directories-first --icons
 end
 
 if command -q git
-    abbr -a gs git status
+    # ghostscript gs is the worst name for me
+    abbr -a -- gs  git status --short --branch
+    abbr -a -- gss git status --branch --find-renames
+    abbr -a -- gco git checkout
 end
