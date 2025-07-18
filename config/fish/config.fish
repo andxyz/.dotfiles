86e3a61daadb611d933e3a7dcd4ec58d93349437
Andrew Stevens <andxyz.stevens@gmail.com>
2025-07-18 16:12:09 -0400

    Cleanup fish config
    - add some useful aliases and cargo tools
    - fixup rbenv in fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,26 +2,63 @@ if status is-interactive
     test -d "$HOME/.cargo/bin" &&
         export PATH="$HOME/.cargo/bin:$PATH"
 
-    # starship
-    # https://github.com/starship/starship
+    # cargo install --locked zellij carapace starship git-delta eza zoxide
 
-    # carapace-bin
-    # https://carapace-sh.github.io/carapace-bin/carapace-bin.html
+    #-----------
+    # PATHS
+    #-----------
+    set -gx PATH  "$HOME/.local/bin" $PATH;
 
     # fisher install jorgebucaran/fisher
-    # fisher install carapace-sh/carapace-bin
 
     # setup starship prompt
     # https://starship.rs/config/
     starship init fish | source
 
+    # carapace-bin
+    # https://carapace-sh.github.io/carapace-bin/carapace-bin.html
+    # fisher install carapace-sh/carapace-bin
+
     # setup z command
     # https://github.com/ajeetdsouza/zoxide?tab=readme-ov-file#installation
-    zoxide init fish | source
+    # zoxide init fish | source
 
-    # setup fzf key bindings 
+    # setup fzf key bindings
     # https://github.com/junegunn/fzf?tab=readme-ov-file#setting-up-shell-integration
-    fzf --fish | source # note: needed fzf --version 0.64
+    # fzf --fish | source # note: needed fzf --version 0.64
 
-    # cargo install --locked zellij carapace starship git-delta eza
+    #-----------
+    # Prompt
+    #-----------
+    # Do not show any greeting
+    set --universal --erase fish_greeting
+    function fish_greeting; end
+    funcsave fish_greeting
+
+    #-----------
+    # Vars
+    #-----------
+    # Modify our path to include our Go binaries
+    contains $HOME/code/go/bin $fish_user_paths; or set -Ua fish_user_paths $HOME/code/go/bin
+    contains $HOME/bin $fish_user_paths; or set -Ua fish_user_paths $HOME/bin
+
+    # Exported variables
+    if isatty
+        set -x GPG_TTY (tty)
+    end
+
+    # Editor
+    set -gx EDITOR nvim
+
+    #------------
+    # Functions
+    #------------
+    # Shortcut to setup a nix-shell with fish. This lets you do something like
+    # `fnix -p go` to get an environment with Go but use the fish shell along
+    # with it.
+    abbr -a fnix nix-shell --run fish
+    abbr -a -- refish 'reset; exec env -i fish -li'
+
+    # rbenv setup see ~/.rbenv/bin/rbenv init --help
+    ~/.rbenv/bin/rbenv init - --no-rehash fish | source
 end
