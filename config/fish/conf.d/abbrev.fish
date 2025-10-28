f02ad2f7312d76db861ab6b79fcab16d6f71fc8b
Andrew Stevens <andxyz.stevens@gmail.com>
2025-10-28 10:13:05 -0400

    Add gd, git ddiffc is too much to type, lol

--- config/fish/conf.d/abbrev.fish
+++ config/fish/conf.d/abbrev.fish
@@ -2,7 +2,8 @@
 
 if command -q git
     # ghostscript gs is the worst name for me
-    abbr -a -- gs  git status --short --branch
+    abbr -a -- gs  git status --short --branch --find-renames
     abbr -a -- gss git status --branch --find-renames
+    abbr -a -- gd  git ddiffc
     abbr -a -- gco git checkout
 end
